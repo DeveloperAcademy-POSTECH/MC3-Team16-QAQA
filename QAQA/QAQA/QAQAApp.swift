731dd47bbd25f2607e75669bc8a867357ece81cf
kpk0616 <kpk0616@naver.com>
2023-07-10 12:04:54 +0900

    [Init] 프로젝트 Init

---
+++ QAQA/QAQA/QAQAApp.swift
@@ -0,0 +1,17 @@
+//
+//  QAQAApp.swift
+//  QAQA
+//
+//  Created by 박의서 on 2023/07/10.
+//
+
+import SwiftUI
+
+@main
+struct QAQAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
