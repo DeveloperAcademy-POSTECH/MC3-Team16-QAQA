e09b6bd23ce6e51b0d19188b58d1848ddc30569e
Molly <131835522+hyelinkim@users.noreply.github.com>
2023-07-11 11:53:39 +0900

    [Add] : QuestionMain 파일 생성
    QuestionMain 파일을 생성

---
+++ QAQA/QAQA/View/QuestionMain.swift
@@ -0,0 +1,20 @@
+//
+//  QuestionMainView.swift
+//  QAQA
+//
+//  Created by 김혜린 on 2023/07/11.
+//
+
+import SwiftUI
+
+struct QuestionMain: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct QuestionMain_Previews: PreviewProvider {
+    static var previews: some View {
+        QuestionMain()
+    }
+}
