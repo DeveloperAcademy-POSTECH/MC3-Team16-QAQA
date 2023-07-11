4d559a4fa2240582353c1ce26a99bed613c30b45
Molly <131835522+hyelinkim@users.noreply.github.com>
2023-07-11 12:13:07 +0900

    [Feat] : 질문 힌트 버튼 생성

--- QAQA/QAQA/View/QuestionMain.swift
+++ QAQA/QAQA/View/QuestionMain.swift
@@ -8,8 +8,18 @@
 import SwiftUI
 
 struct QuestionMain: View {
+    
+    @State private var showModal = false
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack{
+            Button("Easy") {
+                showModal = true
+            }
+            Button("Hard") {
+                
+            }
+        }
     }
 }
 
