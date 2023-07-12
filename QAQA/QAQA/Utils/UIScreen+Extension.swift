066d99f348fb4163d94cb9ac648de382bf6228b4
Kiyoung <rlaky45@naver.com>
2023-07-12 11:52:35 +0900

    [Add] : UIScreen+Extension 추가

---
+++ QAQA/QAQA/Utils/UIScreen+Extension.swift
@@ -0,0 +1,17 @@
+//
+//  UIScreen+Extension.swift
+//  QAQA
+//
+//  Created by 김기영 on 2023/07/12.
+//
+
+import Foundation
+import UIKit
+
+extension UIScreen {
+    
+    static let width = UIScreen.main.bounds.width
+    static let height = UIScreen.main.bounds.height
+
+
+}
