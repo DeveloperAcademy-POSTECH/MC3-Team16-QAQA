b8bc2a27a1840739b8e30789d852c4f021490a64
Kiyoung <rlaky45@naver.com>
2023-07-12 22:32:24 +0900

    [Feat]#4 - TimerModalView UI 구현

--- QAQA/QAQA/View/TimerModalView.swift
+++ QAQA/QAQA/View/TimerModalView.swift
@@ -9,8 +9,44 @@ import SwiftUI
 
 struct TimerModalView: View {
     @EnvironmentObject var timerModel: TimerModel
+    @State var timeContainerWidth = UIScreen.width * 0.31
+    @State var paddingHeight = UIScreen.height * 0.04
+    @State var restartButtonColor: Color = Color.blue.opacity(0.8)
     var body: some View {
-        Text("\(timerModel.countMin):\(timerModel.countSecond)")
+        VStack(alignment: .center){
+            if timerModel.countSecond < 10 {
+                Text("\(timerModel.countMin):0\(timerModel.countSecond)").font(.system(size: 45).bold())
+                    .foregroundColor(.black)
+                    .frame(width: timeContainerWidth)
+                    .padding(.bottom)
+                        .frame(height: paddingHeight)
+            }
+            else {
+                Text("\(timerModel.countMin):\(timerModel.countSecond)").font(.system(size: 45).bold())
+                    .foregroundColor(.black)
+                    .frame(width: timeContainerWidth)
+                    .padding(.bottom)
+                        .frame(height: paddingHeight)
+            }
+            Text("일시정지")
+                .frame(width: timeContainerWidth)
+                .padding(.bottom, paddingHeight * 1.3)
+                    
+            Button(action: {}, label: {
+                ZStack{
+                    RoundedRectangle(cornerRadius: 20)
+                        .foregroundColor(restartButtonColor)
+                        .frame(width: UIScreen.width * 0.8, height: UIScreen.height * 0.07)
+                    Text("재개하기")
+                        .fontWeight(.heavy)
+                        .foregroundColor(.white)
+                }
+            })
+            .padding(.bottom,paddingHeight * 0.3)
+            Button(action: {}, label: {
+                Text("종료하기").foregroundColor(restartButtonColor) // 종료하기를 누르면 어디로 가는지?
+            })
+        }
     }
 }
 
