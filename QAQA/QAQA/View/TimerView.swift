fa512f520d719e8694bc45ada7585a4c7179676d
Kiyoung <rlaky45@naver.com>
2023-07-12 11:16:00 +0900

    [Add] :  TimerView 생성

---
+++ QAQA/QAQA/View/TimerView.swift
@@ -0,0 +1,25 @@
+//
+//  TimerView.swift
+//  QAQA
+//
+//  Created by 김기영 on 2023/07/12.
+//
+
+import SwiftUI
+
+struct TimerView: View {
+    var body: some View {
+        ZStack{
+            HStack{
+                Circle().foregroundColor(.blue.opacity(0.2))
+                    
+            }
+        }
+    }
+}
+
+struct TimerView_Previews: PreviewProvider {
+    static var previews: some View {
+        TimerView()
+    }
+}
