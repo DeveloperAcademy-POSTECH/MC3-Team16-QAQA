32deac45f16862483e06d95d68d71f9923148a34
Molly <131835522+hyelinkim@users.noreply.github.com>
2023-07-13 12:20:27 +0900

    [Feat] #3 - HintModal 내 질문힌트 랜덤 표시
    하나의 모달 안에서 힌트 모드를 선택하고 그에 따라 변수가 랜덤으로 나타나도록 함.

--- QAQA/QAQA/View/HintModal.swift
+++ QAQA/QAQA/View/HintModal.swift
@@ -8,10 +8,43 @@
 import SwiftUI
 
 struct HintModal: View {
+    
+    enum HintMode: String, CaseIterable, Identifiable {
+        case easy, hard
+        var id: Self { self }
+    }
+    
+    @State private var selectedHint: HintMode = .easy
+    
+    let easyhints = ["1", "2", "3", "4", "5", "6", "7", "8"]
+    let hardhints = ["11", "22", "33", "44", "55", "66", "77", "88"]
+    
     var body: some View {
-        Text("Hint Modal")
+        VStack{
+            Text("Hint Modal")
+            Picker("HintMode", selection: $selectedHint) {
+                ForEach(HintMode.allCases) { hintMode in
+                    Text(hintMode.rawValue.capitalized)
+                }
+            }
+            .pickerStyle(.segmented)
+            .padding()
+            
+            Text("\(selectedHint.rawValue)")
+            
+            if selectedHint == .easy {
+                if let easyhint = easyhints.randomElement() {
+                    Text("\(easyhint)")
+                }
+            }else {
+                if let hardhint = hardhints.randomElement() {
+                    Text("\(hardhint)")
+                }
+            }
+        }
     }
 }
+
 
 struct HintModal_Previews: PreviewProvider {
     static var previews: some View {
