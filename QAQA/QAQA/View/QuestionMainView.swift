96eec1d9d6666fcf5b250b55f5c598d77c32b817
Molly <131835522+hyelinkim@users.noreply.github.com>
2023-07-13 12:17:53 +0900

    [Del] #3 - EasyModal, HardModal 파일 삭제
    힌트 버튼을 한 개로 줄이고 그에 따라 EasyModal, HardModal 파일을 삭제함.

--- QAQA/QAQA/View/QuestionMainView.swift
+++ QAQA/QAQA/View/QuestionMainView.swift
@@ -9,31 +9,16 @@ import SwiftUI
 
 struct QuestionMainView: View {
     
-    @State private var showEasyModal = false
-    @State private var showHardModal = false
     @State private var showHintModal = false
     
     var body: some View {
         HStack{
-            Button("Easy") {
-                showEasyModal = true
-            }.sheet(isPresented: $showEasyModal) {
-                EasyModal()
-//                    .presentationDetents([.fraction(0.5)])
-                    .presentationDetents([.height(311)])
-                    .presentationCornerRadius(40)
-            }
-            .cornerRadius(100)
-            Button("Hard") {
-                showHardModal = true
-            }.sheet(isPresented: $showHardModal) {
-                HardModal()
-                    .presentationDetents([.large, .medium, .fraction(0.75)])
-            }
             Button("Modal") {
                 showHintModal = true
             }.sheet(isPresented: $showHintModal) {
                 HintModal()
+                    .presentationDetents([.height(311)])
+                    .presentationCornerRadius(40)
             }
         }
     }
