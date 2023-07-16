87104da13dc606bf4613eb96a038d507f462697e
kpk0616 <kpk0616@naver.com>
2023-07-16 16:25:46 +0900

    [Feat] #7 - OutroEndingView UI 구현

--- QAQA/QAQA/QAQAApp.swift
+++ QAQA/QAQA/QAQAApp.swift
@@ -11,7 +11,8 @@ import SwiftUI
 struct QAQAApp: App {
     var body: some Scene {
         WindowGroup {
-            HomeView()
+//            HomeView()
+            OutroEndingView()
                 .environmentObject(TimerModel())
         }
     }
--- QAQA/QAQA/View/Outro/OutroEndingView.swift
+++ QAQA/QAQA/View/Outro/OutroEndingView.swift
@@ -8,53 +8,64 @@
 import SwiftUI
 
 struct OutroEndingView: View {
+    @State private var isShowingInfoView = true
     @State private var userName = "웃쾌마"
     @State private var reactionNum = 24
     
     var body: some View {
-        VStack {
-            Spacer()
-                .frame(height: 63)
-            Text("\(userName) 님,")
-                .font(.system(size: 20))
-                .foregroundColor(.gray)
-            Spacer()
-                .frame(height: 16)
-            Text("\(reactionNum)")
-                .font(.system(size: 80))
-                .fontWeight(.bold)
-            Spacer()
-                .frame(height: 8)
-            Text("개의 반응을 받았어요!")
-                .font(.system(size: 24))
-                .fontWeight(.semibold)
-                .foregroundColor(.gray)
-            Spacer()
-            ScrollView(.horizontal, showsIndicators: false) {
-                HStack {
-                    Rectangle()
-                        .frame(width: 192, height: 246)
-                    Spacer()
-                        .frame(width: 20)
-                    Rectangle()
-                        .frame(width: 192, height: 246)
+        ZStack {
+            VStack {
+                Spacer()
+                    .frame(height: 63)
+                Text("\(userName) 님,")
+                    .font(.system(size: 20))
+                    .foregroundColor(.gray)
+                Spacer()
+                    .frame(height: 16)
+                Text("\(reactionNum)")
+                    .font(.system(size: 80))
+                    .fontWeight(.bold)
+                Spacer()
+                    .frame(height: 8)
+                Text("개의 반응을 받았어요!")
+                    .font(.system(size: 24))
+                    .fontWeight(.semibold)
+                    .foregroundColor(.gray)
+                Spacer()
+                ScrollView(.horizontal, showsIndicators: false) {
+                    HStack {
+                        Rectangle()
+                            .frame(width: 192, height: 246)
+                        Spacer()
+                            .frame(width: 20)
+                        Rectangle()
+                            .frame(width: 192, height: 246)
+                    }
+                    .padding([.leading, .trailing], 16)
+                    .padding([.top, .bottom], 70)
                 }
-                .padding([.leading, .trailing], 16)
-                .padding([.top, .bottom], 70)
+                Spacer()
+                    .frame(height: 10)
+                Button {
+                    // action
+                } label: {
+                    Text("수고하셨습니다!")
+                        .foregroundColor(.white)
+                        .padding([.top, .bottom], 20)
+                        .frame(maxWidth: .infinity)
+                        .background(RoundedRectangle(cornerRadius: 16))
+                }.padding(.bottom, 16)
+                    .padding([.leading, .trailing], 16)
+                
             }
-            Spacer()
-                .frame(height: 10)
-            Button {
-                // action
-            } label: {
-                Text("수고하셨습니다!")
-                    .foregroundColor(.white)
-                    .padding([.top, .bottom], 20)
-                    .frame(maxWidth: .infinity)
-                    .background(RoundedRectangle(cornerRadius: 16))
-            }.padding(.bottom, 16)
-                .padding([.leading, .trailing], 16)
-
+            if (isShowingInfoView) { // TODO: animation
+                OutroInfoView()
+                    .onAppear() {
+                        DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
+                            isShowingInfoView.toggle()
+                        }
+                    }
+            }
         }
     }
 }
