d17c445be20b8a6878c3e799a534735cf9c78424
hosic <128379323+Joy19061618@users.noreply.github.com>
2023-07-17 11:17:39 +0900

    [Fix] Haptic 작동불량 해결 및 코드 수정. 커스텀 햅틱 추가.

--- QAQA/QAQA/Utils/HapticManager/HapticFeedbackManager.swift
+++ QAQA/QAQA/Utils/HapticManager/HapticFeedbackManager.swift
@@ -5,103 +5,197 @@
 //  Created by 조호식 on 2023/07/16.
 //
 
-// SwiftUI, CoreHaptics, UIKit을 import합니다.
-// CoreHaptics는 고급 햅틱 피드백을 생성하기 위한 프레임워크이며,
-// UIKit는 알림 햅틱을 생성하기 위한 프레임워크입니다.
 import SwiftUI
 import CoreHaptics
 import UIKit
 
-// HapticFeedbackManager 클래스는 햅틱 피드백을 관리합니다.
 class HapticFeedbackManager: ObservableObject {
-    // CHHapticEngine 인스턴스를 저장합니다.
-    // 이 엔진은 햅틱 피드백 이벤트를 생성하고 재생하는데 사용됩니다.
-    private var engine: CHHapticEngine?
+    var engine: CHHapticEngine?
     
-    // 생성자에서는 햅틱 지원 여부를 체크하고, 엔진을 초기화하며 시작합니다.
-    init() {
-        // 현재 장치가 햅틱 기능을 지원하는지 확인합니다.
+
+    func simpleSuccess() {
+        let generator = UINotificationFeedbackGenerator()
+        generator.notificationOccurred(.success)
+    }
+    
+    func simpleError() {
+        let generator = UINotificationFeedbackGenerator()
+        generator.notificationOccurred(.error)
+    }
+    
+    func simpleWarning() {
+        let generator = UINotificationFeedbackGenerator()
+        generator.notificationOccurred(.warning)
+    }
+    
+    
+    
+    //커스텀을 위한엔진 .onAppear(perform: hapticManager.prepareHaptics) 를 같이 넣어줘야함
+    func prepareHaptics() {
         guard CHHapticEngine.capabilitiesForHardware().supportsHaptics else { return }
+        
         do {
-            // 엔진을 초기화하고 시작합니다.
             engine = try CHHapticEngine()
             try engine?.start()
         } catch {
-            // 에러가 발생하면 콘솔에 출력합니다.
-            print("There was an error creating the engine: \(error.localizedDescription)")
+            print("there was an error creating the engine: \(error.localizedDescription)")
         }
     }
+    //커스텀을 위한엔진 끝
     
-    // 햅틱 피드백 이벤트를 생성하고 재생합니다.
-    // 이 메소드는 강도(float 형식)의 배열을 인자로 받아, 각 강도에 해당하는 진동을 순차적으로 재생합니다.
-    func generateHapticFeedback(intensities: [Float]) {
-        // 다시 한 번 햅틱 지원 여부를 확인합니다.
+    
+    
+    //올라갔다~ 내려갔다~
+    func complexMountain() {
+        // make sure that the device supports haptics
         guard CHHapticEngine.capabilitiesForHardware().supportsHaptics else { return }
         
-        // 이벤트를 저장할 배열을 초기화합니다.
         var events = [CHHapticEvent]()
-        // 강도 배열의 각 원소에 대해 반복하며, 해당 강도의 진동 이벤트를 생성합니다.
-        for (i, intensity) in intensities.enumerated() {
-            // 강도와 날카로움 파라미터를 설정합니다. 두 파라미터 모두 강도 값을 사용합니다.
-            let intensityParameter = CHHapticEventParameter(parameterID: .hapticIntensity, value: intensity)
-            let sharpnessParameter = CHHapticEventParameter(parameterID: .hapticSharpness, value: intensity)
-            // 이벤트를 생성합니다. 각 이벤트는 0.1초 간격으로 순차적으로 재생됩니다.
-            let event = CHHapticEvent(eventType: .hapticTransient, parameters: [intensityParameter, sharpnessParameter], relativeTime: Double(i) * 0.1)
+        
+        // create one intense, sharp tap
+        for i in stride(from: 0, to: 1, by: 0.1) {
+            let intensity = CHHapticEventParameter(parameterID: .hapticIntensity, value: Float(i))
+            let sharpness = CHHapticEventParameter(parameterID: .hapticSharpness, value: Float(i))
+            let event = CHHapticEvent(eventType: .hapticTransient, parameters: [intensity, sharpness], relativeTime: i)
             events.append(event)
         }
         
+        for i in stride(from: 0, to: 1, by: 0.1) {
+            let intensity = CHHapticEventParameter(parameterID: .hapticIntensity, value: Float(1 - i))
+            let sharpness = CHHapticEventParameter(parameterID: .hapticSharpness, value: Float(1 - i))
+            let event = CHHapticEvent(eventType: .hapticTransient, parameters: [intensity, sharpness], relativeTime: 1 + i)
+            events.append(event)
+        }
+        // convert those events into a pattern and play it immediately
         do {
-            // 이벤트 배열을 바탕으로 패턴을 생성하고, 이 패턴을 재생하는 플레이어를 생성합니다.
             let pattern = try CHHapticPattern(events: events, parameters: [])
             let player = try engine?.makePlayer(with: pattern)
-            // 플레이어를 즉시 시작하여 햅틱 피드백을 재생합니다.
             try player?.start(atTime: 0)
         } catch {
-            // 에러가 발생하면 콘솔에 출력합니다.
-            print("Failed to play pattern: \(error.localizedDescription)")
+            print("Failed to play pattern: \(error.localizedDescription).")
         }
     }
+    //올라갔다~ 내려갔다~ 끝
     
-    // 알림 햅틱 피드백을 생성하고 재생하는 메소드입니다.
-    // 기본값으로 성공(success) 알림을 재생하지만, 다른 알림 유형을 인자로 전달하여 해당 알림을 재생할 수 있습니다.
-    func generateNotificationFeedback(type: UINotificationFeedbackGenerator.FeedbackType = .success) {
-        // UINotificationFeedbackGenerator 인스턴스를 생성합니다.
-        let generator = UINotificationFeedbackGenerator()
-        // 알림 햅틱을 재생합니다.
-        generator.notificationOccurred(type)
-    }
-}
+    
+    
+    //내려갔다~ 올라갔다~
+    func complexValley() {
+        // make sure that the device supports haptics
+        guard CHHapticEngine.capabilitiesForHardware().supportsHaptics else { return }
+        
+        var events = [CHHapticEvent]()
+        
+        // create one intense, sharp tap
+        for i in stride(from: 0, to: 1, by: 0.1) {
+            let intensity = CHHapticEventParameter(parameterID: .hapticIntensity, value: Float(1 - i))
+            let sharpness = CHHapticEventParameter(parameterID: .hapticSharpness, value: Float(1 - i))
+            let event = CHHapticEvent(eventType: .hapticTransient, parameters: [intensity, sharpness], relativeTime: i)
+            events.append(event)
+        }
 
-// HapticButton 뷰는 햅틱 피드백을 재생하는 버튼입니다.
-struct HapticButton: View {
-    // 라벨, 강도 배열, 알림 유형을 저장하는 프로퍼티를 정의합니다.
-    var label: String
-    var intensities: [Float]
-    var notificationType: UINotificationFeedbackGenerator.FeedbackType?
-    // HapticFeedbackManager 인스턴스를 @StateObject 속성으로 저장합니다.
-    // 이 인스턴스는 버튼의 햅틱 피드백을 관리합니다.
-    @StateObject var hapticFeedbackManager = HapticFeedbackManager()
-    
-    // 뷰의 body에서는 버튼을 생성하고, 버튼 클릭 시 햅틱 피드백을 재생하는 액션을 설정합니다.
-    var body: some View {
-        Button(action: {
-            // 알림 유형이 설정되어 있으면, 해당 알림 햅틱을 재생합니다.
-            if let notificationType = notificationType {
-                hapticFeedbackManager.generateNotificationFeedback(type: notificationType)
-            } else {
-                // 알림 유형이 설정되어 있지 않으면, 강도 배열을 바탕으로 햅틱 피드백을 재생합니다.
-                hapticFeedbackManager.generateHapticFeedback(intensities: intensities)
-            }
-        }) {
-            // 버튼의 라벨을 설정합니다.
-            Text(label)
-                // 라벨에 패딩을 추가하고, 배경색을 파란색으로 설정합니다.
-                .padding()
-                .background(Color.blue)
-                // 글자색을 흰색으로 설정합니다.
-                .foregroundColor(.white)
-                // 버튼 모서리를 둥글게 만듭니다.
-                .cornerRadius(10)
+        for i in stride(from: 0, to: 1, by: 0.1) {
+            let intensity = CHHapticEventParameter(parameterID: .hapticIntensity, value: Float(i))
+            let sharpness = CHHapticEventParameter(parameterID: .hapticSharpness, value: Float(i))
+            let event = CHHapticEvent(eventType: .hapticTransient, parameters: [intensity, sharpness], relativeTime: 1 + i)
+            events.append(event)
+        }
+        // convert those events into a pattern and play it immediately
+        do {
+            let pattern = try CHHapticPattern(events: events, parameters: [])
+            let player = try engine?.makePlayer(with: pattern)
+            try player?.start(atTime: 0)
+        } catch {
+            print("Failed to play pattern: \(error.localizedDescription).")
         }
     }
+    //내려갔다~ 올라갔다~ 끝
+    
+    
+    
+    //S.O.S 따따따 따따 따~ 따따따
+    func sosHaptics() {
+        guard CHHapticEngine.capabilitiesForHardware().supportsHaptics else { return }
+        
+        let dot: Float = 0.1
+        let dash: Float = 0.3
+        let gap: Float = 0.1
+        
+        let sosPattern: [Float] = [dot, gap, dot, gap, dot, gap, dash, gap, dash, gap, dash, gap, dot, gap, dot, gap, dot]
+        
+        var events = [CHHapticEvent]()
+        var time: Float = 0
+        
+        for duration in sosPattern {
+            let intensity = CHHapticEventParameter(parameterID: .hapticIntensity, value: 1)
+            let sharpness = CHHapticEventParameter(parameterID: .hapticSharpness, value: 1)
+            
+            let event = CHHapticEvent(eventType: .hapticTransient, parameters: [intensity, sharpness], relativeTime: TimeInterval(time))
+            events.append(event)
+            
+            time += duration
+        }
+        
+        do {
+            let pattern = try CHHapticPattern(events: events, parameters: [])
+            let player = try engine?.makePlayer(with: pattern)
+            try player?.start(atTime: 0)
+        } catch {
+            print("Failed to play pattern: \(error.localizedDescription).")
+        }
+    }
+    //S.O.S 따따따 따따 따~ 따따따 끝
+    
+    
+    
+    //verygood 모스부호
+    func veryGoodHaptics() {
+        guard CHHapticEngine.capabilitiesForHardware().supportsHaptics else { return }
+        
+        let dot: Float = 0.1
+        let dash: Float = 0.3
+        let gap: Float = 0.1
+        let letterGap: Float = 0.3
+        
+        // V . . . -
+        // E .
+        // R . - .
+        // Y - . - -
+        // G - - .
+        // O - - -
+        // O - - -
+        // D - . .
+        let veryGoodPattern: [Float] = [dot, gap, dot, gap, dot, gap, dash, letterGap,
+                                        dot, letterGap,
+                                        dot, gap, dash, gap, dot, letterGap,
+                                        dash, gap, dot, gap, dash, gap, dash, letterGap,
+                                        dash, gap, dash, gap, dot, letterGap,
+                                        dash, gap, dash, gap, dash, letterGap,
+                                        dash, gap, dash, gap, dash, letterGap,
+                                        dash, gap, dot, gap, dot]
+        
+        var events = [CHHapticEvent]()
+        var time: Float = 0
+        
+        for duration in veryGoodPattern {
+            let intensity = CHHapticEventParameter(parameterID: .hapticIntensity, value: 1)
+            let sharpness = CHHapticEventParameter(parameterID: .hapticSharpness, value: 1)
+            
+            let event = CHHapticEvent(eventType: .hapticTransient, parameters: [intensity, sharpness], relativeTime: TimeInterval(time))
+            events.append(event)
+            
+            time += duration
+        }
+        
+        do {
+            let pattern = try CHHapticPattern(events: events, parameters: [])
+            let player = try engine?.makePlayer(with: pattern)
+            try player?.start(atTime: 0)
+        } catch {
+            print("Failed to play pattern: \(error.localizedDescription).")
+        }
+    }
+    //verygood 모스부호 끝
+
+
 }
--- QAQA/QAQA/Utils/HapticManager/HapticTestView.swift
+++ QAQA/QAQA/Utils/HapticManager/HapticTestView.swift
@@ -7,33 +7,47 @@
 
 import SwiftUI
 
-// HapticTestView는 여러 햅틱 피드백을 재생하는 버튼들을 보여주는 뷰입니다.
 struct HapticTestView: View {
+    @ObservedObject var hapticManager = HapticFeedbackManager()
+    
     var body: some View {
-        // VStack을 사용하여 수직으로 여러 버튼들을 배열합니다.
-        VStack {
-            // 각각 다른 햅틱 피드백을 재생하는 HapticButton들을 생성합니다.
-            
-            // 강도가 1.0인 진동을 한 번 재생하는 버튼
-            HapticButton(label: "Button 1", intensities: [1.0])
-            // 강도가 0.8인 진동을 두 번 재생하는 버튼
-            HapticButton(label: "Button 2", intensities: [0.8, 0.8])
-            // 강도가 0.6인 진동을 세 번 재생하는 버튼
-            HapticButton(label: "Button 3", intensities: [0.6, 0.6, 0.6])
-            // '성공' 알림 햅틱을 재생하는 버튼
-            HapticButton(label: "Success Button", intensities: [], notificationType: .success)
-            // '경고' 알림 햅틱을 재생하는 버튼
-            HapticButton(label: "Warning Button", intensities: [], notificationType: .warning)
-            // '오류' 알림 햅틱을 재생하는 버튼
-            HapticButton(label: "Error Button", intensities: [], notificationType: .error)
-        }
+        Text("simpleSuccess")
+            .padding()
+            .onTapGesture(perform: hapticManager.simpleSuccess)
+        
+        Text("simpleError")
+            .padding()
+            .onTapGesture(perform: hapticManager.simpleError)
+        
+        Text("simpleWarning")
+            .padding()
+            .onTapGesture(perform: hapticManager.simpleWarning)
+        
+        Text("complexMountain")
+            .padding()
+            .onAppear(perform: hapticManager.prepareHaptics)
+            .onTapGesture(perform: hapticManager.complexMountain)
+        
+        Text("complexValley")
+            .padding()
+            .onAppear(perform: hapticManager.prepareHaptics)
+            .onTapGesture(perform: hapticManager.complexValley)
+        
+        Text("sosHaptics")
+            .padding()
+            .onAppear(perform: hapticManager.prepareHaptics)
+            .onTapGesture(perform: hapticManager.sosHaptics)
+        
+        Text("veryGoodHaptics")
+            .padding()
+            .onAppear(perform: hapticManager.prepareHaptics)
+            .onTapGesture(perform: hapticManager.veryGoodHaptics)
+        
+        
     }
 }
 
-// 미리보기를 제공하는 프로토콜 PreviewProvider를 준수하는 구조체입니다.
 struct HapticTestView_Previews: PreviewProvider {
-    // 미리보기를 위한 뷰를 반환하는 previews 속성을 정의합니다.
-    // 이 경우 HapticTestView를 미리보기 뷰로 사용합니다.
     static var previews: some View {
         HapticTestView()
     }
