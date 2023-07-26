f7fef281a7a546e618e4c06bea9be84d06a14fac
hosic <128379323+Joy19061618@users.noreply.github.com>
2023-07-26 09:07:05 +0900

    [Fix]#28 - 자이로스코프에서 랜덤홀 추가

--- QAQA/QAQA/Utils/Gyroscopes/BallContainerView.swift
+++ QAQA/QAQA/Utils/Gyroscopes/BallContainerView.swift
@@ -8,17 +8,16 @@
 import SwiftUI
 import CoreHaptics
 
-// BallContainerView는 공의 움직임을 다루는 뷰입니다.
 struct BallContainerView: View {
-    // gyroscopeManager를 관찰하고 있습니다. 이것은 공의 움직임에 필요한 가속도 데이터를 제공합니다.
+    // 자이로스코프 매니저를 관찰합니다. 이는 공의 움직임에 필요한 가속도 데이터를 제공합니다.
     @ObservedObject var gyroscopeManager: GyroscopeManager
 
     // 공의 현재 위치를 나타내는 변수입니다.
     @State private var gyroBallPosition: CGPoint = .zero
     // 공의 현재 속도를 나타내는 변수입니다.
     @State private var gyroVelocity: CGPoint = .zero
-    // 공이 중심에 마지막으로 진입한 시간을 저장하는 변수입니다.
-    @State private var gyroLastEnteredCenter: Date? = nil
+    // 공이 원의 테두리에 마지막으로 닿은 시간을 저장하는 변수입니다.
+    @State private var gyroLastTouchedBorder: Date? = nil
     // 햅틱 엔진에 대한 참조를 저장하는 변수입니다.
     @State private var gyroEngine: CHHapticEngine?
     // 원 안에 공이 머물러 있는 시간을 계산하는 카운트다운을 나타내는 변수입니다.
@@ -31,21 +30,22 @@ struct BallContainerView: View {
     @State private var gyroCircleLineWidth: CGFloat = 2
     // 배경색상을 나타내는 변수입니다.
     @State private var gyroBackgroundColor: Color = Color.black
-
     // 공의 이미지를 나타내는 변수입니다.
     @State private var gyroBallImage: String = "boom_1"
     // 배경 이미지를 나타내는 변수입니다.
     @State private var gyroBackgroundImage: String = "back_1"
-
     // 랜덤한 시간 간격을 나타내는 변수입니다.
     @State private var gyroRandomTime: Double = Double.random(in: 10...20)
     // 랜덤한 시간 간격을 관리하는 타이머입니다.
     @State private var gyroRandomTimer: Timer? = nil
+    // 공이 화면에 보이는지 나타내는 변수를 추가합니다.
+    @State private var isBallVisible: Bool = true
+    // 원의 위치를 나타내는 변수입니다.
+    @State private var circlePosition: CGPoint = .zero
 
     // 공의 움직임에 영향을 주는 가속도 인자입니다.
     let gyroAccelerationFactor: CGFloat = 0.2
 
-    // 뷰의 본문을 정의하는 부분입니다.
     var body: some View {
         // GeometryReader를 사용하여 부모 뷰의 크기와 위치에 접근합니다.
         GeometryReader { geometry in
@@ -56,22 +56,26 @@ struct BallContainerView: View {
                     .resizable()
                     .edgesIgnoringSafeArea(.all)
 
-                // 원을 그립니다.
+                // 원을 그립니다. 원의 위치는 `circlePosition` 변수에 의해 결정됩니다.
                 Circle()
                     .stroke(gyroCircleColor, lineWidth: gyroCircleLineWidth)
                     .frame(width: 100, height: 100)
+                    .position(self.circlePosition == .zero ?
+                              CGPoint(x: geometry.size.width / 2, y: geometry.size.height / 2) :
+                              self.circlePosition)
 
-                // 공을 그립니다.
+                // 공을 그립니다. 공의 위치는 `gyroBallPosition` 변수에 의해 결정됩니다.
                 Ball(gyroImage: self.$gyroBallImage)
-                    // 공의 위치를 설정합니다.
+                    .opacity(self.isBallVisible ? 1.0 : 0.0)  // 공의 가시성을 설정합니다.
                     .position(self.gyroBallPosition == .zero ?
                               CGPoint(x: geometry.size.width / 2, y: geometry.size.height / 2) :
                               self.gyroBallPosition)
-                    // 뷰가 나타날 때 실행되는 코드입니다.
                     .onAppear {
-                        // 햅틱을 준비하고, 공의 초기 위치를 설정하고, 랜덤 타이머를 시작합니다.
+                        // 뷰가 나타날 때 실행되는 코드입니다.
+                        // 햅틱을 준비하고, 공과 원의 초기 위치를 설정하고, 랜덤 타이머를 시작합니다.
                         prepareHaptics()
                         self.gyroBallPosition = CGPoint(x: geometry.size.width / 2, y: geometry.size.height / 2)
+                        self.circlePosition = CGPoint(x: geometry.size.width / 2, y: geometry.size.height / 2)
                         self.gyroRandomTimer = Timer.scheduledTimer(withTimeInterval: self.gyroRandomTime, repeats: true) { _ in
                             // 랜덤 타이머가 실행될 때마다 공의 이미지와 배경 이미지를 변경하고, 햅틱 피드백을 제공합니다.
                             self.gyroBallImage = "boom_2"
@@ -116,14 +120,14 @@ struct BallContainerView: View {
                             }
 
                             // 공이 원 안에 있으면 원의 색상과 두께를 변경하고, 카운트다운을 시작합니다.
-                            if sqrt(pow(self.gyroBallPosition.x - geometry.size.width / 2, 2) + pow(self.gyroBallPosition.y - geometry.size.height / 2, 2)) <= 100 {
+                            if sqrt(pow(self.gyroBallPosition.x - self.circlePosition.x, 2) + pow(self.gyroBallPosition.y - self.circlePosition.y, 2)) <= 100 {
                                 gyroCircleColor = Color.blue
                                 gyroCircleLineWidth = 8
 
-                                if let gyroLastEnteredCenter = self.gyroLastEnteredCenter {
-                                    // 공이 원 안에 3초 이상 머물렀으면 배경색을 변경하고, 공의 이미지를 변경하고, 햅틱 피드백을 제공합니다.
-                                    if gyroLastEnteredCenter.addingTimeInterval(3) <= Date() {
-                                        self.gyroLastEnteredCenter = nil
+                                if let gyroLastTouchedBorder = self.gyroLastTouchedBorder {
+                                    // 공이 원의 테두리에 3초 이상 닿아있으면 배경색을 변경하고, 공의 이미지를 변경하고, 햅틱 피드백을 제공합니다.
+                                    if gyroLastTouchedBorder.addingTimeInterval(3) <= Date() {
+                                        self.gyroLastTouchedBorder = nil
                                         self.successHaptics()
                                         self.gyroCountdownTimer?.invalidate()
                                         self.gyroCountdown = 0
@@ -131,6 +135,20 @@ struct BallContainerView: View {
                                         gyroBackgroundColor = Color.purple
                                         self.gyroBallImage = "smile_1"
                                         self.gyroBackgroundImage = "sky_1"
+
+                                        // 공을 숨기고 0.5초 후에 랜덤한 위치에 재생성합니다.
+                                        self.isBallVisible = false
+                                        DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+                                            self.isBallVisible = true
+                                            self.gyroBallPosition = CGPoint(
+                                                x: CGFloat.random(in: 50...(geometry.size.width - 50)),
+                                                y: CGFloat.random(in: 50...(geometry.size.height - 50))
+                                            )
+                                            self.circlePosition = CGPoint(
+                                                x: CGFloat.random(in: 50...(geometry.size.width - 50)),
+                                                y: CGFloat.random(in: 50...(geometry.size.height - 50))
+                                            )
+                                        }
                                         // 2초 후에 공의 이미지와 배경 이미지를 원래대로 돌리고, 랜덤 시간 간격을 재설정합니다.
                                         DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
                                             self.gyroBallImage = "boom_1"
@@ -139,8 +157,8 @@ struct BallContainerView: View {
                                         }
                                     }
                                 } else {
-                                    // 공이 원 안에 처음 들어올 때 카운트다운을 시작합니다.
-                                    self.gyroLastEnteredCenter = Date()
+                                    // 공이 원의 테두리에 처음 닿을 때 카운트다운을 시작합니다.
+                                    self.gyroLastTouchedBorder = Date()
                                     self.gyroCountdown = 3
                                     self.gyroCountdownTimer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { _ in
                                         self.gyroCountdown -= 1
@@ -151,7 +169,7 @@ struct BallContainerView: View {
                                 }
                             } else {
                                 // 공이 원을 벗어나면 카운트다운을 중지하고, 원의 색상과 두께를 원래대로 돌립니다.
-                                self.gyroLastEnteredCenter = nil
+                                self.gyroLastTouchedBorder = nil
                                 self.gyroCountdownTimer?.invalidate()
                                 self.gyroCountdown = 0
                                 gyroCircleColor = Color.white
@@ -165,7 +183,7 @@ struct BallContainerView: View {
                 // 원 안에 머물러 있는 시간을 화면에 표시합니다.
                 if gyroCountdown > 0 {
                     Text("\(gyroCountdown)")
-                        .font(.largeTitle)
+                        .font(.system(size: 50, weight: .bold))  // 폰트 크기를 50으로, 볼드체로 변경합니다.
                         .foregroundColor(.white)
                         .position(x: geometry.size.width / 2, y: geometry.size.height / 2)
                 }
@@ -182,7 +200,7 @@ struct BallContainerView: View {
             self.gyroEngine = try CHHapticEngine()
             try gyroEngine?.start()
         } catch {
-            print("There was an error creating the engine: \(error.localizedDescription)")
+            print("햅틱 엔진 생성 중 오류 발생: \(error.localizedDescription)")
         }
     }
 
@@ -205,7 +223,7 @@ struct BallContainerView: View {
             let player = try gyroEngine?.makePlayer(with: pattern)
             try player?.start(atTime: 0)
         } catch {
-            print("Failed to play pattern: \(error.localizedDescription).")
+            print("패턴 재생 실패: \(error.localizedDescription).")
         }
     }
 }
