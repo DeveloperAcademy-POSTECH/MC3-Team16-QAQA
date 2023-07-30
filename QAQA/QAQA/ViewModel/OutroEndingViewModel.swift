6dd0653c2f0029466829d0d964247265cc113fbc
kpk0616 <kpk0616@naver.com>
2023-07-30 17:50:32 +0900

    [Del] #39 - OutroEndingModel 삭제

--- QAQA/QAQA/ViewModel/OutroEndingViewModel.swift
+++ QAQA/QAQA/ViewModel/OutroEndingViewModel.swift
@@ -8,18 +8,13 @@
 import Foundation
 
 class OutroEndingViewModel: ObservableObject {
-//    private var outroEndingModel = OutroEndingModel()
-    
     func calculateKingjungWidth(kingjung: CGFloat, total: CGFloat) -> CGFloat {
-//        return (outroEndingModel.totalKingjung/outroEndingModel.totalReaction)*360
         return (kingjung/total)*360
     }
     func calculateEvaWidth(eva: CGFloat, total: CGFloat) -> CGFloat {
         return (eva/total)*360
-//        return (outroEndingModel.totalEva/outroEndingModel.totalReaction)*360
     }
     func calculateSpacerWidth(kingjung: CGFloat, eva: CGFloat, total: CGFloat) -> CGFloat {
-//        return 360-(calculateKingjungWidth()+calculateEvaWidth())
         return 360 - ((kingjung/total)*360 + (eva/total)*360)
     }
 }
