2f238b9b675de282aa0424209aff98a49f512b37
Hyelin Kim <jasmine6470@gmail.com>
2023-07-31 09:27:11 +0900

    [Feat] #52 - BeginQuestionModalView UI 틀 구현
    틀만 구현

--- QAQA/QAQA/View/Question/BeginQuestionModalView.swift
+++ QAQA/QAQA/View/Question/BeginQuestionModalView.swift
@@ -8,6 +8,9 @@
 import SwiftUI
 
 struct BeginQuestionModalView: View {
+    
+    @ObservedObject var game: RealTimeGame
+    
     var body: some View {
         VStack{
             Text("질문 폭격")
@@ -15,13 +18,16 @@ struct BeginQuestionModalView: View {
                 Image(systemName: "alarm.fill")
                 Text("10분")
             }
-            Image("")
+            Image("beginQuestionModalQaqa")
+            Text("이제 \(game.topicUserName)에게 질문폭격을 해볼 차례에요! 준비됐나요?")
+            Text("소리와 함께 하면 더 즐겁게 즐길 수 있어요!")
+            Image("startButton")
         }
     }
 }
 
 struct BeginQuestionModalView_Previews: PreviewProvider {
     static var previews: some View {
-        BeginQuestionModalView()
+        BeginQuestionModalView(game:RealTimeGame())
     }
 }
