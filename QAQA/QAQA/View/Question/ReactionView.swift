83f44d3f1ef3f1d867312af2fef282ec65b56423
kpk0616 <kpk0616@naver.com>
2023-07-31 12:41:17 +0900

    [Del] #60 - ReactionView 리액션한 사람 이름 코드 삭제

--- QAQA/QAQA/View/Question/ReactionView.swift
+++ QAQA/QAQA/View/Question/ReactionView.swift
@@ -24,15 +24,6 @@ struct ReactionView: View {
                     ReactionLottieView(jsonName:"badReactionLottie")
                         .frame(width: 390, height: 390)
                 }
-                Text("by Your Teammates") //리액션을 누를 사람의 이름 뜨는 곳
-                    .font(.system(size:20))
-                    .foregroundColor(.gray)
-                    .opacity(0)
-                game.myAvatar
-                    .resizable()
-                    .frame(width: 80, height: 80)
-                    .clipShape(Circle())
-                    .opacity(0)
             }
             .frame(height: 541)
         }
