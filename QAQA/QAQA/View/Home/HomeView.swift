07f461bb21f5f5b2099a2c2a082be5194cd07090
Kiyoung <rlaky45@naver.com>
2023-08-01 14:32:16 +0900

    [Chore]#45 - 매칭 후 퀘스쳔뷰가 나오도록 되돌림

--- QAQA/QAQA/View/Home/HomeView.swift
+++ QAQA/QAQA/View/Home/HomeView.swift
@@ -59,14 +59,14 @@ struct HomeView: View {
         }
         // Display the game interface if a match is ongoing.
         .fullScreenCover(isPresented: $game.playingGame) {
-//            QuestionView(game:game)
-//                .onAppear(){
-//                    gameTimerModel.countMin = 10
-//                    gameTimerModel.countSecond = 0
-//                    gameTimerModel.isTimer = true
-//                }
+            QuestionView(game:game)
+                .onAppear(){
+                    gameTimerModel.countMin = 10
+                    gameTimerModel.countSecond = 0
+                    gameTimerModel.isTimer = true
+                }
 //            TestingIntroGame(game: game)
-            TestIntroStartView(game: game)
+//            TestIntroStartView(game: game)
              
         }
     }
